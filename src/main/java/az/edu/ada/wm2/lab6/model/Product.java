b760ee752c9979b133f188023bdd249850e7883f
bmuradova17974 <Bmuradova17974@ada.edu.az>
2026-04-01 20:36:52 +0400

    Add ProductRepository

--- src/main/java/az/edu/ada/wm2/lab6/model/Product.java
+++ src/main/java/az/edu/ada/wm2/lab6/model/Product.java
@@ -1,10 +1,17 @@
 package az.edu.ada.wm2.lab6.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 import java.util.UUID;
 
+@Entity
+@Table(name="products")
 public class Product {
+    @Id
     private UUID id;
     private String productName;
     private BigDecimal price;
